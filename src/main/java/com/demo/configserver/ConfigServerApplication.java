04b640720a956f177328ff2838a8acbc5624ece2
saba <sabasaeedakhtar@gmail.com>
2024-02-23 10:47:54 +1100

    added few documentation lines

--- src/main/java/com/demo/configserver/ConfigServerApplication.java
+++ src/main/java/com/demo/configserver/ConfigServerApplication.java
@@ -14,3 +14,21 @@ public class ConfigServerApplication {
 	}
 
 }
+/**************************************************************************************************************************************
+ * Below are main projects of Spring Cloud
+ * Spring Cloud Config: provides centralized configuration for client as well as server side in a distributed system . like we did in 
+ * this app
+ * Spring Cloud Consul: Helps in configuration management and Service discovery from HashiCorp Consul 
+ * Spring Cloud Gateway : Provides library to implement API gateway on the top of Spring WebMVC or Spring Webflux
+ * Spring Cloud Netflix: Integrarion with various Netflix OSS (Eureka , Hysterix and Zuul)
+ * Spring Cloud Zookeeper: Apache Zookeper helps in configuration management and Service discovery
+ * 
+ * Features of Spring cloud(lightweight microservices framework) : 
+ * - centralized configuration in a distributed system 
+ * - Service registration and discovery 
+ * - routing
+ * - Load balancing 
+ * - Circuit Breakers
+ * - Asynchronous messaging
+ *
+**************************************************************************************************************************************/
